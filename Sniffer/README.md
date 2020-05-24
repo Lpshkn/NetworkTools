ac2eea7d8b89e86dee682fd42033b524d8d9001e
lpshkn <kofffsh@gmail.com>
2020-05-24 15:20:28 +0300

    Add the sniffer, third party libs, source files etc
    Add the sniffer program

---
+++ Sniffer/README.md
@@ -0,0 +1,3 @@
+# Sniffer
+
+The program to sniff the network traffic and print information about frames, packets etc.
---
+++ Sniffer/src/main.cpp
@@ -0,0 +1,12 @@
+#include <pcap.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <errno.h>
+#include <sys/types.h>
+#include <inaddr.h>
+
+#define ARP 0x0806
+#define IPv4 0x0800
+
