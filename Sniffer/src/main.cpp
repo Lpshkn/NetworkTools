23a2bd65044b096169e97b1d5f003e9d521283fa
Nikshepel <kofffsh@gmail.com>
2020-06-03 16:04:48 +0300

    Update the main module
    Add information about program, add configurator object

--- Sniffer/src/main.cpp
+++ Sniffer/src/main.cpp
@@ -1,7 +1,17 @@
 #include <iostream>
+#include <vector>
 #include <tins/tins.h>
+#include "configurator.h"
 
-int main() {
+
+int main(int argc, char* argv[]) {
+    auto programName = "Sniffer";
+    auto description = "The program to sniff the network traffic and print information about frames, packets etc.";
+    auto epilog = "Lpshkn, 2020";
+    auto version = "0.1.0";
+
+    auto configurator = Configurator(programName, description,
+                                     epilog, version).parseArguments(argc, argv);
 
     return 0;
 }
