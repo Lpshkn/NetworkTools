08336e487269f68a1044f50d1374fda8ec43e480
Lpshkn <kofffsh@gmail.com>
2020-06-04 00:59:30 +0300

    Update the main function
    Add building a Sniffer object and printing information about it

--- Sniffer/src/main.cpp
+++ Sniffer/src/main.cpp
@@ -1,7 +1,6 @@
 #include <iostream>
-#include <vector>
-#include <tins/tins.h>
 #include "configurator.h"
+#include "sniff.h"
 
 
 int main(int argc, char* argv[]) {
@@ -11,7 +10,18 @@ int main(int argc, char* argv[]) {
     auto version = "0.1.0";
 
     auto configurator = Configurator(programName, description,
-                                     epilog, version).parseArguments(argc, argv);
+                                     epilog, version);
+    configurator.parseArguments(argc, argv);
+    auto sniffer = buildSniffer(configurator);
+
+    int number = 1;
+    for (auto &packet : *sniffer) {
+        std::cout << "Packet number: " << number << std::endl;
+
+        auto pdu = packet.pdu();
+        printPdu(pdu);
+        number++;
+    }
 
     return 0;
 }
