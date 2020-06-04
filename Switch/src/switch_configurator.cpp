af9a9fae15d9595930d6e9d33a510f976b63ee2e
Lpshkn <kofffsh@gmail.com>
2020-06-04 22:18:20 +0300

    Add SwitchConfigurator class to configure arguments of the command line
    ParseArguments method was edited because of didn't throw an exception when no interfaces were passed.

---
+++ Switch/src/switch_configurator.cpp
@@ -0,0 +1,61 @@
+#include <memory>
+#include <utility>
+#include <regex>
+#include "switch_configurator.h"
+
+// Method to creating an instance of ArgumentParser to process arguments by the command line
+std::unique_ptr<argparse::ArgumentParser> SwitchConfigurator::buildParser() {
+    auto parser = std::make_unique<argparse::ArgumentParser>(programName_, version_);
+    parser->add_description(description_);
+    parser->add_epilog(epilog_);
+
+    parser->add_argument("interfaces")
+            .help("the network interfaces which will be sniffed and will be involved to process of the switch")
+            .remaining();
+
+    parser->add_argument("-t", "--time")
+            .help("time of a note life (in seconds) in the commutation table, by default it's 60 seconds")
+            .default_value(60)
+            .action([](const std::string& value) { return std::stoi(value); });
+
+    return parser;
+}
+
+SwitchConfigurator::SwitchConfigurator(const std::string& programName, const std::string& description,
+                                         const std::string& epilog, const std::string& version) noexcept
+        : Configurator(programName, description, epilog, version) {
+    initialize();
+}
+
+// Method to initialize important values in the constructor
+void SwitchConfigurator::initialize() {
+    argumentParser_ = std::move(buildParser());
+}
+
+std::unique_ptr<std::vector<std::string>> SwitchConfigurator::getInterfaces() {
+    auto it = argumentParser_->get<std::vector<std::string>>("interfaces");
+
+    return std::make_unique<std::vector<std::string>>(it);
+}
+
+void SwitchConfigurator::parseArguments(int argc, char **argv) {
+    try {
+        argumentParser_->parse_args(argc, argv);
+        auto ifaces = argumentParser_->get<std::vector<std::string>>("interfaces");
+        if (ifaces.size() < 2) {
+            throw std::runtime_error("There are less than 2 interfaces were passed");
+        }
+    }
+    catch (const std::runtime_error& err) {
+        std::string error = err.what();
+        error = std::regex_replace(error, std::regex {"^[\\s:]+"}, "");
+        std::cerr << "Error: " << error << std::endl;
+        std::cerr << argumentParser_->help().str() << std::endl;
+        exit(-1);
+    }
+    catch (const std::logic_error& err) {
+        std::cerr << "Error: No interfaces were passed" << std::endl;
+        std::cerr << argumentParser_->help().str() << std::endl;
+        exit(-5);
+    }
+}
---
+++ Switch/src/switch_configurator.h
@@ -0,0 +1,22 @@
+#ifndef NETWORKTOOLS_SWITCH_CONFIGURATOR_H
+#define NETWORKTOOLS_SWITCH_CONFIGURATOR_H
+
+#include <argparse/argparse.hpp>
+#include <tins/tins.h>
+#include "configurator.h"
+
+class SwitchConfigurator : public Configurator {
+public:
+    explicit SwitchConfigurator() = delete;
+    explicit SwitchConfigurator(const std::string& program_name, const std::string& description = {},
+                                 const std::string& epilog = {}, const std::string& version = {}) noexcept;
+
+    std::unique_ptr<std::vector<std::string>> getInterfaces();
+    void parseArguments(int argc, char** argv) override;
+
+private:
+    std::unique_ptr<argparse::ArgumentParser> buildParser() override;
+    void initialize();
+};
+
+#endif //NETWORKTOOLS_SWITCH_CONFIGURATOR_H
