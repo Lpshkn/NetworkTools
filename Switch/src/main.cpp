55db56f1cf1f92ea7612c5e4032f46919c80d118
Lpshkn <kofffsh@gmail.com>
2020-06-04 22:22:13 +0300

    Add the main function to Switch

--- Switch/src/main.cpp
+++ Switch/src/main.cpp
@@ -1,5 +1,14 @@
+#include "switch_configurator.h"
 
 int main(int argc, char** argv) {
+    auto programName = "Switch";
+    auto description = "The program receive and redirect packets to certain ports.";
+    auto epilog = "Lpshkn, 2020";
+    auto version = "0.1.0";
+
+    auto configurator = SwitchConfigurator(programName, description,
+                                            epilog, version);
+    configurator.parseArguments(argc, argv);
 
     return 0;
 }
