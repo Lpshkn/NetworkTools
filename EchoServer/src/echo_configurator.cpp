d52993a88926ef19774f6b857a1a0bdba586191a
Lpshkn <kofffsh@gmail.com>
2020-06-10 16:35:23 +0300

    Add Configurator for the EchoServer module

---
+++ EchoServer/src/echo_configurator.cpp
@@ -0,0 +1,58 @@
+#include <fstream>
+#include "echo_configurator.h"
+
+EchoConfigurator::EchoConfigurator(const std::string &program_name, const std::string &description,
+                                   const std::string &epilog, const std::string &version) noexcept
+                                   : Configurator(program_name, description, epilog, version) {
+    initialize();
+}
+
+std::unique_ptr<argparse::ArgumentParser> EchoConfigurator::buildParser() {
+    auto parser = std::make_unique<argparse::ArgumentParser>(programName_, version_);
+    parser->add_description(description_);
+    parser->add_epilog(epilog_);
+
+    parser->add_argument("-s", "--server_port")
+            .help("the port of the server")
+            .action([](const std::string& value) { return std::stoi(value); });
+
+    parser->add_argument("-c", "--client_port")
+            .help("the port of the client")
+            .action([](const std::string& value) { return std::stoi(value); });
+
+    parser->add_argument("-a", "--address")
+            .help("the ip address of the server")
+            .action([](const std::string& value) { return Tins::IPv4Address(value); });
+
+    parser->add_argument("-f", "--config_file")
+            .help("the file containing settings to configure program")
+            .action([](const std::string& value) { return value; });
+
+    return parser;
+}
+
+void EchoConfigurator::initialize() {
+    argumentParser_ = std::move(buildParser());
+}
+
+uint16_t EchoConfigurator::getServerPort() {
+    return getPort("-s");
+}
+
+uint16_t EchoConfigurator::getClientPort() {
+    return getPort("-c");
+}
+
+Tins::IPv4Address EchoConfigurator::getServerAddress() {
+    return argumentParser_->get<Tins::IPv4Address>("-a");
+}
+
+uint16_t EchoConfigurator::getPort(const std::string& argument) {
+    auto port = argumentParser_->get<int>(argument);
+    if (port <= 0 || port > 65535) {
+        std::cerr << "Error: port is incorrect" << std::endl;
+        std::cerr << *argumentParser_ << std::endl;
+        exit(-1);
+    }
+    return port;
+}
---
+++ EchoServer/src/echo_configurator.h
@@ -0,0 +1,23 @@
+#ifndef NETWORKTOOLS_ECHO_CONFIGURATOR_H
+#define NETWORKTOOLS_ECHO_CONFIGURATOR_H
+
+#include "configurator.h"
+
+class EchoConfigurator : public Configurator {
+public:
+    explicit EchoConfigurator() = delete;
+    explicit EchoConfigurator(const std::string& program_name, const std::string& description = {},
+                              const std::string& epilog = {}, const std::string& version = {}) noexcept;
+
+    uint16_t getServerPort();
+    uint16_t getClientPort();
+    Tins::IPv4Address getServerAddress();
+
+private:
+
+    std::unique_ptr<argparse::ArgumentParser> buildParser() override;
+    uint16_t getPort(const std::string& argument);
+    void initialize();
+};
+
+#endif
