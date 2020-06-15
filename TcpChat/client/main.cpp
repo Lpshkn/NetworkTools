1ddb4463cc6845f8f44b57b7997ec8543a26e532
Lpshkn <kofffsh@gmail.com>
2020-06-15 23:22:13 +0300

    Update the main function of the Client in TcpChat
    Now the user must input his nickname

--- TcpChat/client/main.cpp
+++ TcpChat/client/main.cpp
@@ -7,5 +7,33 @@ int main(int argc, char* argv[]) {
     auto epilog = "Lpshkn, 2020";
     auto version = "0.1.0";
 
+    ChatConfigurator configurator(programName, description, epilog, version);
+    configurator.parseArguments(argc, argv);
+
+    Client client(configurator);
+
+    std::cout << "If you want to exit, just input: \"exit\"" << std::endl << std::endl;
+
+    std::string nickname;
+    std::cout << "Enter your nickname: ";
+    std::cin >> nickname;
+    std::cout << std::endl;
+
+    while(true) {
+        std::string message;
+        std::cin >> message;
+
+        if (message == "exit")
+            break;
+
+        try {
+            client.sendMessage(message);
+        }
+        catch (const std::logic_error &err) {
+            std::cerr << err.what() << std::endl;
+            exit(-100);
+        }
+    }
+
     return 0;
 }
